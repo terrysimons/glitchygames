8dd7cd40005b3ce2c7fd64903e4da93cae9bb5c5
Terry Simons <terry.simons@gmail.com>
2021-12-21 13:56:30 -0800

    Migrate to newer package setup format

--- setup.py
+++ setup.py
@@ -1,22 +1,4 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="ghettogames",
-    version="0.0.1",
-    author="Terry Simons",
-    author_email="terry.simons@gmail.com",
-    description="Ghetto Games and Tools for Pygame and Python 3",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/terrysimons/ghettogames",
-    packages=setuptools.find_packages(),
-    scripts=['scripts/bitmappy'],
-    classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-    ],
-)
+if __name__ == "__main__":
+    setuptools.setup()
