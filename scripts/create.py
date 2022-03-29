a3f54638ac52137c238095fb3405cc6ebf57c029
Rich Saupe <sabadam32@users.noreply.github.com>
2022-03-29 16:45:34 -0500

    refactor: added argparsing to cli.  added docstrings (#7)

--- scripts/create.py
+++ scripts/create.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
+"""
+Create Glitchy Games template game.
+"""
+import argparse
 from glitchygames import templates
-import sys
 
 
-if len(sys.argv) > 1 and sys.argv[-1].lower() == 'pong':
-    templates.build('pong')
+def get_args():
+    """Parse command line arguments"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--list', action='store_true',  required=False, help='list available templates')
+    parser.add_argument('template', choices=templates.get_templates(), nargs='?', help='The template to use')
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+
+    if args.list:
+        [print(x) for x in templates.get_templates()]
+    elif args.template == 'pong':
+        templates.build('pong')
+
+
+if __name__ == '__main__':
+    main()
