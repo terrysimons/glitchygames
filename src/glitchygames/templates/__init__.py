3699de7e981b9c1f7069eb7fb66234af1bc5a48d
Rich Saupe <rich@formswift.com>
2022-03-29 16:45:13 -0500

    refactor: added argparsing to cli.  added docstrings

--- src/glitchygames/templates/__init__.py
+++ src/glitchygames/templates/__init__.py
@@ -4,5 +4,12 @@ import os.path
 path = os.path.dirname(__file__)
 
 
+def get_templates():
+    """Returns a list of templates"""
+    contents = os.listdir(path)
+    return [x for x in contents if os.path.isdir(os.path.join(path, x)) and not x.startswith('__')]
+
+
 def build(template):
+    """Builds the project from the template"""
     cookiecutter(os.path.join(path, template))
